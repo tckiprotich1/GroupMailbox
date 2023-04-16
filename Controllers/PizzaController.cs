10371bab334745ab17ea97a19308fb00e312872d
COLLINS  TONUI <tckiprotich@gmail.com>
2023-04-16 08:43:01 +0000

    bcontrollerbase creation

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,26 @@
+using GroupMailbox.Models;
+using Microsoft.AspNetCore.Mvc;
+using GroupMailbox.Services;
+
+namespace GroupMailbox.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    // Get all action
+
+    // get by id action
+
+    // create action
+
+    // update action
+
+    // delete action
+
+}
