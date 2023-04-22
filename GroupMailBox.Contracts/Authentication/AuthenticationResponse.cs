736aefcadf7ade5d3c12fdcb733d627eb39a39e2
COLLINS  TONUI <tckiprotich@gmail.com>
2023-04-22 05:30:20 +0000

    pr

---
+++ GroupMailBox.Contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,12 @@
+namespace GroupMailBox.Contracts.Authentication
+{
+    public record AuthenticationResponse
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }        
+        // public string? Password { get; set; }
+        // public string? ConfirmPassword { get; set; }
+    }
+}
---
+++ GroupMailBox.Contracts/Authentication/LoginRequest.cs
@@ -0,0 +1,10 @@
+namespace GroupMailBox.Contracts.Authentication
+{
+    public record LoginRequest
+    {
+        public string? Email { get; set; }
+               
+        public string? Password { get; set; }
+        // public string? ConfirmPassword { get; set; }
+    }
+}
